26c7444c20ab99d2889c419bdfabd1b7e67e3731
Ananda Rizki Amelia <spicaonge@gmail.com>
2024-11-13 10:00:47 +0700

    feat: :zap: Added validation and error handling to AuthorController, and implemented rules and custom validation in Author model.

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Author;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AuthorController extends Controller
 {
@@ -31,7 +32,19 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), Author::rules('insert'));
+        Author::customValidation($validator);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->messages()], 400);
+        }
+        try {
+            $data = Author::create($request->all());
+
+            return response()->json(['message' => 'Data berhasil disimpan', 'data' => $data], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -39,7 +52,13 @@ class AuthorController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $data = Author::find($id);
+
+            return response()->json(['data' => $data], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -47,7 +66,13 @@ class AuthorController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        try {
+            $data = Author::find($id);
+
+            return response()->json(['data' => $data], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -55,7 +80,21 @@ class AuthorController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validator = Validator::make($request->all(), Author::rules('update'));
+        Author::customValidation($validator);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->messages()], 400);
+        }
+
+        try {
+            $data = Author::find($id);
+            $data->update($request->all());
+
+            return response()->json(['message' => 'Data berhasil diupdate', 'data' => null]);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 
     /**
@@ -63,6 +102,13 @@ class AuthorController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $data = Author::find($id);
+            $data->delete();
+
+            return response()->json(['message' => 'Data berhasil dihapus'], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
     }
 }
--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Validation\Validator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,11 +13,52 @@ class Author extends Model
     protected $fillable = [
         'name',
         'birthdate',
-        
+
     ];
 
-public function books()
-{
-    return $this->hasMany(Book::class);
-}
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    /**
+     * Aturan validasi untuk model ini.
+     *
+     * @return array
+     */
+    public static function rules($process)
+    {
+        if ($process == 'insert') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        } elseif ($process == 'update') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        }
+    }
+
+    /**
+     * Mendaftarkan aturan validasi kustom.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public static function customValidation(Validator $validator)
+    {
+        $customAttributes = [
+            'name' => 'Nama',
+            'birthdate' => 'Tanggal Lahir',
+        ];
+        $validator->addReplacer('required', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus diisi.');
+        });
+
+        $validator->addReplacer('date', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus berupa tanggal.');
+        });
+    }
 }
